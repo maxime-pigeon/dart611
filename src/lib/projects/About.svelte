31066e5eae2ba00eb5d1e1c462de20f21a05276b
Maxime Pigeon <me@maximepigeon.com>
2021-04-13 17:17:32 -0400

    adjust spacing

--- src/lib/projects/About.svelte
+++ src/lib/projects/About.svelte
@@ -1,32 +1,42 @@
 <style>
 	p:first-of-type {
-		margin-top: 1rem;
+		margin-top: 2rem;
 	}
 </style>
 
 <p>
-	This class is a practice-based research studio centered on such active
-	processes of making and bringing something into being from idea and concept
-	to material reality. Through development of the major assignment and all
-	other course-related activities, it is hoped that you will develop
-	strategies of interdisciplinarity, ontological design and engagement to
-	further your own studio work and future praxis.
+	In January of 2021, 15 <a
+		href="https://www.concordia.ca/finearts/design/programs/graduate/design.html"
+		>Master of Design</a
+	> students began gathering on Thursday afternoons on Zoom, clustered on screen,
+	but socially distanced in our lounges, offices, kitchens and bedrooms for remote
+	class during the Covid-19 Pandemic. It was the end of a long and isolated pandemic
+	winter in Montreal where most of the students were based, but many had never
+	met each other off-screen. For a student at home in India we noticed it was always
+	nighttime in the window behind her, her neighborhood quiet and sleeping. For
+	another student, deep in the valleys of France it was also getting late. The
+	professor’s dog frequently barked. Sometimes, children, family members or work
+	mates wandered past in the background of people’s screens, a window into worlds.
+	A companion bird flew about freely in one living room, tweeting comments that
+	we assumed were supportive. Over 13 weeks together, we discussed Interdisciplinary
+	Practices in Design. We opened each class meeting by sharing our highlights of
+	the week: sometimes small moments, sometimes difficult, sometimes pivotal. There
+	was much discussion of kombucha, plants, fonts, and the difficulties of accessing
+	materials and processes during the pandemic. We shared resources and insights
+	from our various design disciplines; spanning architecture, fashion, textiles,
+	interior design, graphic design, landscape architecture, publishing, UX design,
+	gaming, and product design.
 </p>
+
 <p>
-	This exercise is a first step towards defining an outcome for the final
-	project. The Project Proposal differs in significant ways from an artist
-	statement or reflective paper, in that it should communicate in lay terms
-	the physical and material qualities of the creative work. Focus on the
-	format, medium, dimensions, spatial/environmental requirements, technical
-	specifications, and any additional information to help visualize the final
-	outcome of the production. Of course, it is also important to provide a
-	general overview of the theoretical dimensions of the project themes,
-	contextual framework, and historical references, as it relates to
-	contemporary art and culture. Identify and discuss influences, flex, and
-	flux from interdisciplinary thinking and ontological design processes.
-	Consider this 750-1000-word text like an exhibition proposal for a gallery
-	or a commercial pitch to a client. Use whichever or whatever medium and
-	method you would use in your industry to present a new concept. Address the
-	preliminary skill sets, materials, resources and facilities that will need
-	to be considered throughout the development process.
+	This class is a community. The projects that resulted are in dialogue with
+	each other, reflecting the designer’s interactions and conversations, and
+	the ebb and flow of extending and unravelling ideas across disciplines.
+</p>
+
+<p>
+	—Dr Miranda Smitheram, Professor of DART 611 Interdisciplinary Practices in
+	Design, Winter 2021, <a href="https://www.concordia.ca/finearts/design.html"
+		>Department of Design and Computation Arts</a
+	>, Concordia University.
 </p>
